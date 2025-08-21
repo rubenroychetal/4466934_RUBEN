07287b0336798f8f432b753f80279f9f57ddb2af
Your Name <you@example.com>
2025-08-21 17:11:08 +0530

    w4

---
+++ Hackerrank/week4/legoblocks.c
@@ -0,0 +1,207 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+char** split_string(char*);
+
+int parse_int(char*);
+
+#define MOD 1000000007
+
+
+int power(int base, int exp) {
+    long long result = 1;
+    long long b = base;
+    while (exp > 0) {
+        if (exp % 2 == 1)
+            result = (result * b) % MOD;
+        b = (b * b) % MOD;
+        exp /= 2;
+    }
+    return (int)result;
+}
+
+int legoBlocks(int n, int m) {
+    int ways[m + 1];
+    ways[0] = 1;
+
+
+    for (int i = 1; i <= m; i++) {
+        ways[i] = 0;
+        for (int j = 1; j <= 4 && j <= i; j++) {
+            ways[i] = (ways[i] + ways[i - j]) % MOD;
+        }
+    }
+
+    
+    int total[m + 1];
+    for (int i = 1; i <= m; i++) {
+        total[i] = power(ways[i], n);
+    }
+
+    
+    int stable[m + 1];
+    stable[0] = 0;
+    for (int i = 1; i <= m; i++) {
+        stable[i] = total[i];
+        for (int j = 1; j < i; j++) {
+            stable[i] = (stable[i] - ((long long)stable[j] * total[i - j]) % MOD + MOD) % MOD;
+        }
+    }
+
+    return stable[m];
+}
+
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    int t = parse_int(ltrim(rtrim(readline())));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        char** first_multiple_input = split_string(rtrim(readline()));
+
+        int n = parse_int(*(first_multiple_input + 0));
+
+        int m = parse_int(*(first_multiple_input + 1));
+
+        int result = legoBlocks(n, m);
+
+        fprintf(fptr, "%d\n", result);
+    }
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
